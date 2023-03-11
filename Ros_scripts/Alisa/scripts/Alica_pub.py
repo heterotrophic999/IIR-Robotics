2f2efb129fa7169f9090f21325874fd343561586
Melovan <mrdanhik@gmail.com>
2023-03-11 13:19:38 +0700

    Changing command publisher

--- Ros_scripts/Alisa/scripts/Alica_pub.py
+++ Ros_scripts/Alisa/scripts/Alica_pub.py
@@ -5,24 +5,41 @@ import pandas as pd
 import pymongo
 
 def get_responce(client):
-  db = client.get_default_database()
-  docs = pd.DataFrame(db.get_collection('logs').find())
-  str = docs.move[-1:].iloc[0]
-  return str
+    db = client.get_default_database()
+    docs = pd.DataFrame(db.get_collection('logs').find())
+    try:
+        with db.watch(
+                [{'$match': {'operationType': 'insert'}}]) as stream:
+            for insert_change in stream:
+                rospy.loginfo(insert_change)
+    except pymongo.errors.PyMongoError:
+    # The ChangeStream encountered an unrecoverable error or the
+    # resume attempt failed to recreate the cursor.
+       rospy.loginfo("error")
+    return str
+  
 
 def talker():
     MONGODB_URI = 'mongodb+srv://mrdanhik:0321@loser.2mlzec4.mongodb.net/alice?retryWrites=true&w=majority'
     client = pymongo.MongoClient(MONGODB_URI)  # тут должен быть ваш адрес mongo
+    db = client.get_default_database()
     pub = rospy.Publisher('alice', UInt8, queue_size=10)
     rospy.init_node('alice_pub', anonymous=True)
     rate = rospy.Rate(1) # 10hz
-    while not rospy.is_shutdown():
-        rospy.loginfo("Подключение...")
-        move = get_responce(client)
-        rospy.loginfo("Получил данные")
-        rospy.loginfo(move)
-        pub.publish(move)
-        rate.sleep()
+    rospy.loginfo("Подключение...")
+    try:
+        with db.watch(
+                [{'$match': {'operationType': 'insert'}}]) as stream:
+            for insert_change in stream:   
+                rospy.loginfo("Получил данные")
+                docs = pd.DataFrame(db.get_collection('logs').find())
+                move = docs.move[-1:].iloc[0]
+                rospy.loginfo(move)
+                pub.publish(move)
+    except pymongo.errors.PyMongoError:
+# The ChangeStream encountered an unrecoverable error or the
+# resume attempt failed to recreate the cursor.
+        rospy.loginfo("error")
 
 if __name__ == '__main__':
     try:
